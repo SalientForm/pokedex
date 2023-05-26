18184d978c0b76cc9b96f22e757f6c3fa15c1318
Kevin Tamlyn <ktamlyn@gmail.com>
2023-05-26 12:07:00 -0400

    add unit tests to PokedexSearch

--- src/app/pokedex-search/pokedex-search.spec.tsx
+++ src/app/pokedex-search/pokedex-search.spec.tsx
@@ -1,18 +1,52 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import * as module from 'module';
 import { Provider } from 'react-redux';
-import { rootStore } from '../state/root-store';
+import * as pokemonIndexSlice from '../state/pokemon-index/pokemon-index.slice';
+import { PokedexDispatch, rootStore } from '../state/root-store';
 import PokedexSearch from './pokedex-search';
+import { PokedexSearchResultProps, } from './search-result/pokedex-search-result';
 
-vi.mock('./pokedex-search-form/pokedex-search-form');
-vi.mock('./search-result/pokedex-search-result');
+vi.mock('../state/pokemon-index/pokemon-index.slice',  async () => {
+  const actual = await vi.importActual(
+    '../state/pokemon-index/pokemon-index.slice'
+  ) as module;
+  return {
+    ...actual,
+    fetchAllPokemonIndex: () => ({
+      type: 'pokemonIndex/fetchStatus',
+    }),
+  };
+});
+
+vi.mock('./search-result/pokedex-search-result', () => ({
+  default: (props: PokedexSearchResultProps) => <div data-testid="pokedexSearchResult">{props.searchText}</div>
+}));
 
 describe('PokedexSearch', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(
+  it('should render and dispatch fetchAllPokemonIndex successfully', () => {
+    rootStore.dispatch = vi.fn() as PokedexDispatch;
+    render(
       <Provider store={rootStore}>
         <PokedexSearch />
       </Provider>
     );
-    expect(baseElement).toBeTruthy();
+    expect(screen.getByText('Search')).toBeInTheDocument();
+    expect(rootStore.dispatch).toHaveBeenCalledTimes(1);
+    expect(rootStore.dispatch).toHaveBeenCalledWith(
+      pokemonIndexSlice.fetchAllPokemonIndex()
+    );
+  });
+
+  it('updates search text when a new value is inputted', () => {
+    render(
+      <Provider store={rootStore}>
+        <PokedexSearch />
+      </Provider>
+    );
+
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'bulb' },
+    });
+    expect(screen.getByTestId('pokedexSearchResult').textContent).toEqual('bulb');
   });
 });
---
+++ src/app/state/pokemon-index/pokemon-index.seed.ts
@@ -0,0 +1,14 @@
+// seed data for development and testing
+
+export const seedPokemonIndexState1 = {
+  ids: [1],
+  entities: {
+    '1': {
+      name: 'bulbasaur',
+      url: 'https://pokeapi.co/api/v2/pokemon/1/',
+      id: 1,
+    },
+  },
+  loadingStatus: 'loaded',
+  error: '',
+};
